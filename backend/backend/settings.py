5ad1efe3a4001570a9258b4ae043094cf2c3918d
TenSzalik <gracjanchudziak@gmail.com>
2023-02-28 03:53:41 +0100

    feat: Add globally default permission policy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,6 +30,9 @@ if DEBUG:
     ]
 
 REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
