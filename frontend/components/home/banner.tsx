c0e7914b41188ba472d1c3dbcb23f7fc2fcc0dce
TenSzalik <gracjanchudziak@gmail.com>
2023-03-03 04:12:30 +0100

    feat: Init frontend

---
+++ frontend/components/home/banner.tsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+    return (
+        <div>
+            Test
+        </div>
+    )
+};
+
+export default Banner;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import Banner from "../components/home/banner"
+
+function App() {
+  return (
+    <div className="bg-blue-500">
+      <Banner/>
+    </div>
+  )
+}
+
+export default App
