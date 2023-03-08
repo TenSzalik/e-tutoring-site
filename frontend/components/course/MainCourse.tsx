ffcb12060bf5a00deff20c2e1ad958ce8c5b04f0
TenSzalik <gracjanchudziak@gmail.com>
2023-03-08 05:20:15 +0100

    feat: Add course component

---
+++ frontend/components/course/MainCourse.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useRef } from 'react'
+
+const MainCourse = () => {
+    return (
+        <div>
+            <h1>( ͡° ͜ʖ ͡°)</h1>
+        </div>
+    )
+}
+
+export default MainCourse;
--- frontend/components/home/banner.tsx
+++
@@ -1,9 +0,0 @@
-const Banner = () => {
-    return (
-        <div>
-            Test
-        </div>
-    )
-};
-
-export default Banner;
