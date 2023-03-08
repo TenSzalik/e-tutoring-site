222d0d56147837cba012f974ebfc3550e82d4885
TenSzalik <gracjanchudziak@gmail.com>
2023-03-08 05:12:01 +0100

    feat: Set readable files for tailwind & fix screens

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./utils/*.ts",
+    "./components/home/*.tsx",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)'},
+        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)'},
+      }
+    },
   },
   plugins: [],
 }
