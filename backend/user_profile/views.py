c3abd237346931ecbd5beb514de5aa937e8f8f34
TenSzalik <gracjanchudziak@gmail.com>
2023-03-08 05:01:56 +0100

    feat: Set permissions for user

--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -1,10 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import UserCreateSerializer, UserReadSerializer
+from rest_framework.permissions import BasePermission, IsAuthenticated
+
+
+class ReadCreateOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in ('GET', 'HEAD', 'OPTIONS', 'POST')
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
+    # permission_classes = [IsAuthenticated|ReadCreateOnly]
 
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
