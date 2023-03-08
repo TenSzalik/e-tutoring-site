c83fc0de2f535a60e14ae8034e145845ec2413a4
TenSzalik <gracjanchudziak@gmail.com>
2023-03-08 04:58:44 +0100

    feat: Add login & refresh token JWT

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,6 +9,7 @@ from drf_spectacular.views import (
 from rest_framework import routers
 from tutorial.urls import router as tutorial
 from user_profile.urls import router as user_profile
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 router = routers.DefaultRouter()
@@ -18,6 +19,8 @@ urlpatterns = [
     path("api/tutorial/", include(tutorial.urls)),
     path("api/user_profile/", include(user_profile.urls)),
     path("admin/", admin.site.urls),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
