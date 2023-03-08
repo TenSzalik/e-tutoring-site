cafc9a7dcf20b4e2c29d48f8b5feca6e113543d6
TenSzalik <gracjanchudziak@gmail.com>
2023-03-08 05:05:51 +0100

    feat: Add React's Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
-import Banner from "../components/home/banner"
+import MainHome from '../components/home/MainHome'
+import MainCourse from '../components/course/MainCourse'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+
   return (
-    <div className="bg-blue-500">
-      <Banner/>
+    <div>
+      <Routes>
+          <Route path="/" element={<MainHome />} />
+          <Route path='/courses' element={<MainCourse/>} />
+      </Routes>
     </div>
   )
 }
