7ab95a5c407883fdaf26a2bcc24decb29268b48a
TenSzalik <gracjanchudziak@gmail.com>
2023-03-16 23:29:53 +0100

    feat: Add urls chest

---
+++ frontend/utils/apiUrls.ts
@@ -0,0 +1,10 @@
+const host = 'http://0.0.0.0:8000/api/'
+
+const urls = {
+  user: host + 'user/',
+  tutorial: host + 'tutorial/',
+  token: host + 'token/',
+  token_refresh: host + 'token/refresh/',
+}
+
+export default urls
