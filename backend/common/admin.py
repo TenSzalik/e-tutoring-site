45571202cc46a4d6c1283883c796b1f4a10a80ff
TenSzalik <gracjanchudziak@gmail.com>
2023-03-16 22:55:27 +0100

    feat: Add BaseComment & BaseStar models

--- backend/common/admin.py
+++ backend/common/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from common.models import Place
+from common.models import Place, Disabled, BaseComment, BaseStar
 
-admin.site.register(Place)
+admin_classes = [Place, Disabled, BaseComment, BaseStar]
+
+admin.site.register(admin_classes)
--- backend/common/models.py
+++ backend/common/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 from backend.models import BaseModel
 
 
@@ -24,3 +25,22 @@ class Disabled(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class BaseComment(BaseModel):
+    content = models.TextField(max_length=1024, validators=[MinLengthValidator(5)])
+
+
+class BaseStar(BaseModel):
+    CHOICES = [
+        ("1", "1"),
+        ("2", "2"),
+        ("3", "3"),
+        ("4", "4"),
+        ("5", "5"),
+    ]
+
+    stars = models.CharField(max_length=1, choices=CHOICES, default=1)
+
+    def __str__(self):
+        return self.stars
