0131b4b30ea2176f7b541bce350f0c96dac4fab3
TenSzalik <gracjanchudziak@gmail.com>
2023-03-16 23:23:01 +0100

    feat: Add context

--- frontend/components/home/Login.tsx
+++ frontend/components/home/Login.tsx
@@ -1,36 +1,37 @@
-const Login = ({ email, setEmail, password, setPassword, handleSubmit }) => {
+import { useContext } from 'react'
+import AuthContext from '../context/AuthContext'
 
+const Login = () => {
+  const { loginUser } = useContext(AuthContext)
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="p-2">
-        <input
-          type="text"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          className="w-full rounded-lg border-b-2 border-blue-500 p-2 outline-none text-center"
-          placeholder="Email"
-        />
-      </div>
-      <div className="p-2">
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          className="w-full rounded-lg border-b-2 border-blue-500 p-2 outline-none text-center"
-          placeholder="Password"
-        />
-      </div>
-      <div className="p-2">
-        <div className="m-3">
-            <button
-              className="w-32 bg-white tracking-wide text-gray-800 font-bold rounded-lg border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
-              <span className="mx-auto">Login</span>
-            </button>
+    <div>
+      <form onSubmit={loginUser}>
+        <div className="p-2">
+          <input
+            className="w-full rounded-lg border-b-2 border-blue-500 p-2 outline-none text-center"
+            type="email"
+            name="email"
+            placeholder="Enter Username"
+          />
+        </div>
+        <div className="p-2">
+          <input
+            className="w-full rounded-lg border-b-2 border-blue-500 p-2 outline-none text-center"
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+          />
+        </div>
+        <div className="p-2">
+          <div className="m-3">
+            <input
+              className="w-32 bg-white tracking-wide text-gray-800 font-bold rounded-lg border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
+              type="submit"
+            />
           </div>
-      </div>
-    </form>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,37 @@
 import MainHome from '../components/home/MainHome'
 import MainCourse from '../components/course/MainCourse'
-import { Routes, Route } from "react-router-dom";
+import MainUser from '../components/user/MainUser'
+import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from '../components/context/AuthContext'
 
 function App() {
-
   return (
     <div>
       <Routes>
-          <Route path="/" element={<MainHome />} />
-          <Route path='/courses' element={<MainCourse/>} />
+        <Route
+          path="/"
+          element={
+            <AuthProvider>
+              <MainHome />
+            </AuthProvider>
+          }
+        />
+        <Route
+          path="courses"
+          element={
+            <AuthProvider>
+              <MainCourse />
+            </AuthProvider>
+          }
+        />
+        <Route
+          path="profile"
+          element={
+            <AuthProvider>
+              <MainUser />
+            </AuthProvider>
+          }
+        />
       </Routes>
     </div>
   )
