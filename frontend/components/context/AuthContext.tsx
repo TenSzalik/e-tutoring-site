eef186af3f3b26a21fe77440c41bd3b484f0e565
TenSzalik <gracjanchudziak@gmail.com>
2023-06-28 23:22:26 +0200

    fix: AuthToken

--- frontend/components/context/AuthContext.tsx
+++ frontend/components/context/AuthContext.tsx
@@ -1,107 +1,99 @@
-import { createContext, useState, useEffect } from 'react'
-import jwt_decode from 'jwt-decode'
-import { useNavigate } from 'react-router-dom'
-import urls from '../../utils/apiUrls'
+import { createContext, useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import urls from "../../utils/apiUrls";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
-export default AuthContext
+export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
   const [authTokens, setAuthTokens] = useState(() =>
-    localStorage.getItem('authTokens')
-      ? JSON.parse(localStorage.getItem('authTokens'))
+    localStorage.getItem("authTokens")
+      ? JSON.parse(localStorage.getItem("authTokens"))
       : null
-  )
+  );
   const [user, setUser] = useState(() =>
-    localStorage.getItem('authTokens')
-      ? jwt_decode(localStorage.getItem('authTokens'))
+    localStorage.getItem("authTokens")
+      ? jwt_decode(localStorage.getItem("authTokens"))
       : null
-  )
-  const [loading, setLoading] = useState(true)
-  const navigate = useNavigate()
+  );
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   const loginUser = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const response = await fetch(urls.token, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email: event.target.email.value,
         password: event.target.password.value,
       }),
-    })
-    const data = await response.json()
+    });
+    const data = await response.json();
 
     if (response.status === 200) {
-      setAuthTokens(data)
-      setUser(jwt_decode(data.access))
-      localStorage.setItem('authTokens', JSON.stringify(data))
-      navigate('/courses')
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
+      navigate("/courses");
     } else {
-      alert('Something went wrong!')
+      alert("Something went wrong!");
     }
-  }
+  };
 
   const logoutUser = () => {
-    setAuthTokens(null)
-    setUser(null)
-    localStorage.removeItem('authTokens')
-    navigate('/')
-  }
+    setAuthTokens(null);
+    setUser(null);
+    localStorage.removeItem("authTokens");
+    navigate("/");
+  };
 
   const updateToken = async () => {
     const response = await fetch(urls.token_refresh, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ refresh: authTokens.refresh }), //tu jest błąd
-    })
-
-    const data = await response.json()
+      body: JSON.stringify({ refresh: authTokens.refresh }),
+    });
+    const data = await response.json();
 
     if (response.status === 200) {
-      setAuthTokens(data)
-      setUser(jwt_decode(data.access))
-      localStorage.setItem('authTokens', JSON.stringify(data))
-      console.log(JSON.stringify(data))
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
     } else {
-      logoutUser()
+      logoutUser();
     }
 
     if (loading) {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   const contextData = {
     user: user,
     authTokens: authTokens,
     loginUser: loginUser,
     logoutUser: logoutUser,
-  }
+  };
 
   useEffect(() => {
-    if(loading){
-        updateToken()
-        console.log("Dupa")
-    }
-    const fourMinutes = 1000 * 60 * 14
+    const fourteenMinutes = 1000 * 60 * 14;
 
     const interval = setInterval(() => {
       if (authTokens) {
-        updateToken()
+        updateToken();
       }
-    }, fourMinutes)
-    return () => clearInterval(interval)
-  }, [authTokens, loading])
+    }, fourteenMinutes);
+    return () => clearInterval(interval);
+  }, [authTokens, loading]);
 
   return (
-    <AuthContext.Provider value={contextData}>
-      {loading ? null : children}
-    </AuthContext.Provider>
-  )
-}
+    <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
+  );
+};
--- frontend/components/user/Profile.tsx
+++ frontend/components/user/Profile.tsx
@@ -1,22 +1,23 @@
-import { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import AuthContext from '../context/AuthContext'
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 const Profile = () => {
-  const { user, logoutUser } = useContext(AuthContext)
+  const { user, logoutUser } = useContext(AuthContext);
   return (
     <div>
       <Link to="/">Home</Link>
       <span> | </span>
       {user ? (
-        <p onClick={logoutUser}>Logout</p>
+        <div>
+          <p onClick={logoutUser}>Logout</p>
+          <p>Hello {user.user_id}</p>
+        </div>
       ) : (
         <Link to="/login">Login</Link>
       )}
-
-      {user && <p>Hello {user.username}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
